3525796ae1887f49a0757d4b1bc7c37ec99abe8b
ftanlong520 <44928278+ftanlong520@users.noreply.github.com>
2024-08-23 12:47:07 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs.ftianlong520.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.km.ftianlong520.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "PmA5N0dm4QSYesLznUlh3r352ABvdHJYpgLZvOdjdx0=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
